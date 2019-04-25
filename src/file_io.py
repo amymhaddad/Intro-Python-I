524a51f8e4cbd18c1c51606aa2f363ac3e211373
Amy M Haddad <amy.m.haddad@gmail.com>
2019-04-25 10:16:22 -0400

    Adding final verison of ex, file_io

--- src/file_io.py
+++ src/file_io.py
@@ -8,11 +8,20 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 
-# YOUR CODE HERE
+with open("foo.txt", mode="r", encoding="utf-8") as f_object:
+    for content in f_object:
+        print(content.strip())
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make 
 # sure that it contains what you expect it to contain
 
-# YOUR CODE HERE
+with open('bar.txt', mode = 'wt', encoding = 'utf-8') as f_object:
+    f_object.write("My name is Amy.\n")
+    f_object.write("The month is April.\n")
+    f_object.write("The day is Thursday.")
+
+with open('bar.txt', mode = 'rt', encoding = 'utf-8') as f_object:
+    for line in f_object:
+        print(line.strip())
