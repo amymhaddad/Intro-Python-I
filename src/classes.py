61b31fbe3c514c56cb93a83386e946fd577a1558
Amy M Haddad <amy.m.haddad@gmail.com>
2019-04-26 07:53:12 -0400

    adding final version of classes

--- src/classes.py
+++ src/classes.py
@@ -2,10 +2,13 @@
 # constructor
 
 class LatLon():
+
     def __init__(self, lat, lon):
         self.lat = lat
         self.lon = lon
-        
+
+lat_lon = LatLon(30, 40)
+
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon. Look up the `super` method.
 
@@ -13,30 +16,35 @@ class Waypoint(LatLon):
     def __init__(self, name, lat, lon):
         super().__init__(lat, lon)
         self.name = name
+    
+    def __str__(self):
+        return f"\"{self.name}\", {self.lat}, {self.lon}"
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
 
 class Geocache(Waypoint):
-    def __init__(self, difficulty, size, name, lat, lon):
+    def __init__(self, name, difficulty, size, lat, lon):
         super().__init__(name, lat, lon)
         self.difficulty = difficulty
         self.size = size
-
+    
+    def __str__(self):
+        return f"\"{self.name}\", {self.difficulty}, {self.size}, {self.lat}, {self.lon}"
+    
 # Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
 
-location = Waypoint("Catacombs", 41.70505, -121.51521)
-print(location.name + ' ' + str(location.lat) + ' ' + str(location.lon))
+waypoint = Waypoint("Catacombs", 41.70505, -121.51521)
+print(waypoint.name + ' ' + str(waypoint.lat) + ' ' + str(waypoint.lon))
 
 # Without changing the following line, how can you make it print into something
 # more human-readable? Hint: Look up the `object.__str__` method
-# print(waypoint)
-
-
+print(waypoint)
 
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
+geocache = Geocache("Newberry Views", "diff 1.5", "size 2", "44.052137", "-121.41556")
+print(geocache.name, geocache.difficulty, geocache.size, geocache.lat, geocache.lon)
 
-# YOUR CODE HERE
 
 # Print it--also make this print more nicely
-# print(geocache)
+print(geocache)
